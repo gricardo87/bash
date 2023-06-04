29594cb0a6f6d54e70acdd1400d05ffc93b3d9dc
gabEzao <gricardo87@gmail.com>
2023-06-04 10:19:02 -0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -exu
 export DEBIAN_FRONTEND=noninteractive
-PASTASCRIPTS="/etc/scripts/"
 cd $(mktemp -d)
 
 apt-get update && \
@@ -10,20 +9,3 @@ apt-get update && \
 	apt-get autoremove -y && \
 	apt-get autoclean -y &&  \
 	sync
-
-mkdir -p ${PASTASCRIPTS} 2>&-
-
-cat <<EOF > ${PASTASCRIPTS}/upgrade.sh
-#!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
-cd $(mktemp -d)
-apt-get update && \
-	apt-get upgrade -y && \
-	apt-get dist-upgrade -y && \
-	apt-get clean -y && \
-	apt-get autoremove -y && \
-	apt-get autoclean -y &&  \
-	sync
-EOF
-
-chmod +x ${PASTASCRIPTS}/upgrade.sh
