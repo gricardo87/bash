83afe96bd5637bd5668b81a0d2d075cdd7495071
gabEzao <gricardo87@gmail.com>
2023-06-04 08:25:09 -0300

    Update debian-iniital-setup.sh

--- debian-iniital-setup.sh
+++ debian-iniital-setup.sh
@@ -1,31 +1,34 @@
-#!/bin/bash
-# Atualizando os repositórios
+#!/bin/bash -exu
+cd /tmp/
+export DEBIAN_FRONTEND=noninteractive
 apt-get update && \
-    apt-get install -y \
-    net-tools \  # Pacote contendo utilitários de rede, como ifconfig e netstat
-    libxt6 \  # Biblioteca necessária para alguns aplicativos que usam a camada de extensão X Toolkit (XT)
-    libxmu6 \  # Biblioteca necessária para alguns aplicativos que usam a camada de gerenciamento de janelas X (Xmu)
-    bash-completion \  # Complemento de autocompletar para o Bash
-    htop \  # Monitor de recursos interativo
-    rsync \  # Ferramenta de sincronização e transferência de arquivos
-    wget \  # Utilitário para baixar arquivos da web
-    htop \  # Monitor de recursos interativo
-    build-essential \  # Conjunto de pacotes essenciais para compilar software no sistema
-    linux-headers-amd64 \  # Cabeçalhos do kernel Linux para arquitetura amd64
-    vim \  # Editor de texto avançado
-    sudo \  # Permite que usuários não privilegiados executem comandos como superusuários
-    ca-certificates \  # Certificados de autoridade confiáveis
-    curl \  # Ferramenta para transferência de dados com sintaxe de URL
-    apt-transport-https \  # Suporte ao transporte HTTPS para APT
-    gnupg2 \  # Implementação do GnuPG para criptografia e assinatura
-    sudo \  # Permite que usuários não privilegiados executem comandos como superusuários
-    apt-file \  # Ferramenta para pesquisar arquivos em pacotes disponíveis nos repositórios
-    arping \  # Ferramenta de ping avançada que envia pacotes ARP para descobrir e mapear hosts em uma rede local
-    hping3 \  # Ferramenta avançada de verificação e teste de rede
-    mtr-tiny \  # Ferramenta de diagnóstico de rede que combina as funcionalidades do traceroute e do ping
-    awscli \  # Interface de linha de comando da AWS (Amazon Web Services)
-    ntpdate \  # Atualiza o relógio do sistema a partir de servidores NTP
-    bind9-dnsutils  # Pacote contendo utilitários DNS do BIND9
+    apt-get install net-tools \
+    libxt6 \
+    libxmu6 \
+    bash-completion \
+    htop \
+    rsync \
+    wget \
+    htop \
+    build-essential \
+    linux-headers-amd64 \
+    vim \
+    sudo \
+    ca-certificates \
+    curl \
+    apt-transport-https \
+    gnupg2 \
+    sudo \
+    apt-file \
+    arping \
+    hping3 \
+    mtr-tiny \
+    awscli \
+    ntpdate \
+    bind9-dnsutils \
+    aptitude \
+    --no-install-recommends -y
 
-apt-file update
-exit 0
+apt-file update;
+
+echo "[*] Script executado com sucesso!";
