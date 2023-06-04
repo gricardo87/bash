739c71c62133a5470d18261262c1c97def43c468
gabEzao <gricardo87@gmail.com>
2023-06-04 16:51:08 -0300

    Create setup-k8s-debian.sh

---
+++ setup-k8s-debian.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -exu
+export DEBIAN_FRONTEND=noninteractive
+
+# Change to a temporary directory & Download and install Docker
+cd $(mktemp -d)
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+# Change to a temporary directory & Download and install kubectl
+cd $(mktemp -d)
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+mv ./kubectl /usr/local/bin/kubectl
+
+# Change to a temporary directory & Download and install Helm
+cd $(mktemp -d)
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
