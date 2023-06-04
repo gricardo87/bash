1e3ecf6940b9d500aee58dc2fb89d1da5eedd409
gabEzao <gricardo87@gmail.com>
2023-06-04 19:58:41 -0300

    Update setup-k8s-debian.sh

--- setup-k8s-debian.sh
+++ setup-k8s-debian.sh
@@ -7,8 +7,8 @@ rm -f /etc/apt/trusted.gpg.d/kubernetes-xenial.gpg
 #curl -fsSL https://get.docker.com -o get-docker.sh
 #sh get-docker.sh
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/containerd.gpg
-echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu kubernetes-xenial stable" >  /etc/apt/sources.list.d/containerd.list
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/containerd.gpg
+#echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu kubernetes-xenial stable" >  /etc/apt/sources.list.d/kubernetes.list
 apt-get update && apt-get install -y containerd.io
 
 # Change some configurations files and change to a temporary directory & Download and install kubelet kubeadm kubectl
