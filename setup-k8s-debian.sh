b4045f8e0f42843c9937a83d6e8945db5e5a9351
gabEzao <gricardo87@gmail.com>
2023-06-04 19:33:47 -0300

    Update setup-k8s-debian.sh

--- setup-k8s-debian.sh
+++ setup-k8s-debian.sh
@@ -26,7 +26,7 @@ sysctl --system
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
