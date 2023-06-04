568fbc27fd4600ebdf919e249e8c39156f225c6b
gabEzao <gricardo87@gmail.com>
2023-06-04 16:57:47 -0300

    Update setup-k8s-debian.sh

--- setup-k8s-debian.sh
+++ setup-k8s-debian.sh
@@ -17,3 +17,8 @@ cd $(mktemp -d)
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+
+# Change to a temporary directory & Download and install minikube
+cd $(mktemp -d)
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+install minikube-linux-amd64 /usr/local/bin/minikube
