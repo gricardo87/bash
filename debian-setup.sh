544b08ded519693a2b38a53dd3ba203a5a2f2b9c
gabEzao <gricardo87@gmail.com>
2023-06-04 08:33:29 -0300

    Create debian-setup.sh

---
+++ debian-setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -exu
+cd /tmp/
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && \
+    apt-get install net-tools \
+    libxt6 \
+    libxmu6 \
+    bash-completion \
+    htop \
+    rsync \
+    wget \
+    htop \
+    build-essential \
+    linux-headers-amd64 \
+    vim \
+    sudo \
+    ca-certificates \
+    curl \
+    apt-transport-https \
+    gnupg2 \
+    sudo \
+    apt-file \
+    arping \
+    hping3 \
+    mtr-tiny \
+    awscli \
+    ntpdate \
+    bind9-dnsutils \
+    aptitude \
+    --no-install-recommends -y
+
+apt-file update;
+
+echo "[*] Script executado com sucesso!";
