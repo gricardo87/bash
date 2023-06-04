e6b59f9bd6b326b70810a9ecff98c5c69c186fa6
gabEzao <gricardo87@gmail.com>
2023-06-04 20:35:23 -0300

    Update setup-k8s-debian.sh

--- setup-k8s-debian.sh
+++ setup-k8s-debian.sh
@@ -15,7 +15,7 @@ systemctl enable containerd && systemctl restart containerd
 cd $(mktemp -d)
 mkdir -vp /etc/modules-load.d/ /etc/sysctl.d/ 2>&-
 
-cat /etc/modules-load.d/k8s.conf <<EOF
+cat <<EOF > /etc/modules-load.d/k8s.conf
 br_netfilter
 ip_vs
 ip_vs_rr
@@ -25,7 +25,7 @@ nf_conntrack_ipv4
 overlay
 EOF
 
-cat /etc/sysctl.d/kubernetes.conf <<EOF
+cat <<EOF > /etc/sysctl.d/kubernetes.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
