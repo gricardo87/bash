109e302a5f07363c069eef3d85d54790b2144fcd
gabEzao <gricardo87@gmail.com>
2023-09-16 15:56:35 -0300

    Update enable-rclocal.sh

--- enable-rclocal.sh
+++ enable-rclocal.sh
@@ -7,22 +7,22 @@ export unit_file_path="/etc/systemd/system/rc-local.service"
 
 # Create a systemd unit file for rc.local if it doesn't exist
 if [[ !-e $unit_file_path ]]; then
-  cat <<EOF > "$unit_file_path"
-[Unit]
-Description=/etc/rc.local Compatibility
-ConditionPathExists=/etc/rc.local
-
-[Service]
-Type=forking
-ExecStart=/etc/rc.local
-TimeoutSec=0
-StandardOutput=tty
-RemainAfterExit=yes
-SysVStartPriority=99
-
-[Install]
-WantedBy=multi-user.target
-EOF
+  cat <<EOF > $unit_file_path
+  [Unit]
+  Description=/etc/rc.local Compatibility
+  ConditionPathExists=/etc/rc.local
+  
+  [Service]
+  Type=forking
+  ExecStart=/etc/rc.local
+  TimeoutSec=0
+  StandardOutput=tty
+  RemainAfterExit=yes
+  SysVStartPriority=99
+  
+  [Install]
+  WantedBy=multi-user.target
+  EOF
 fi
 
 # Creat and/or set permissions for rc.local file
