6f0854f3fe8268987c4e2f8ebec6e15aa79f0ee4
gabEzao <gricardo87@gmail.com>
2023-11-21 08:38:43 -0300

    Create debian-setup-cloud.sh

---
+++ debian-setup-cloud.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+cd $(mktemp -d)
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && \
+    apt-get install net-tools \
+    git \
+    zstd \
+    nmap \
+    bash-completion \
+    htop \
+    rsync \
+    wget \
+    vim \
+    sudo \
+    ca-certificates \
+    curl \
+    apt-transport-https \
+    gnupg2 \
+    apt-file \
+    arping \
+    hping3 \
+    mtr-tiny \
+    ntpdate \
+    bind9-dnsutils \
+    aptitude \
+    zip \
+    unzip \
+	  awscli --no-install-recommends -y;
+
+apt-file update;
+
+echo "[*] Script executado com sucesso!";
