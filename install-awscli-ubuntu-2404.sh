ec62165f4f833c28b65808e342d515a066725dd9
gabEzao <gricardo87@gmail.com>
2024-09-17 08:09:47 -0300

    Update install-awscli-ubuntu-2404.sh

--- install-awscli-ubuntu-2404.sh
+++ install-awscli-ubuntu-2404.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-PastaTemp=$(mktemp -d)
+export DEBIAN_FRONTEND=noninteractive
+export PASTATEMP=$(mktemp -d)
+
 apt update && \
-apt-get install unzip less curl -y && \
-cd ${PastaTemp} \
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
-unzip awscliv2.zip && \
-./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update && \
-cd /tmp/ && \
-rm -rf ${PastaTemp}
+    apt-get install -y unzip less curl && \
+    cd ${PASTATEMP} && \
+    curl -o "awscliv2.zip" "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" && \
+    unzip awscliv2.zip && \
+    ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update && \
+    cd /tmp && \
+    rm -rf ${PASTATEMP}
+    rm -rf /var/cache/apt/archives /var/lib/apt/lists/* && \
+    apt-get clean &&
+    echo '[*] OK!'
