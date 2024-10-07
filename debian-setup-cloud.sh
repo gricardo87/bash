f7adbdf0582976cf68136a1d8c547ba52ef19df1
gabEzao <gricardo87@gmail.com>
2024-10-06 23:26:29 -0300

    Update debian-setup-cloud.sh

--- debian-setup-cloud.sh
+++ debian-setup-cloud.sh
@@ -35,7 +35,7 @@ cd ${AWSCLITMP} && \
 apt-get update && \
 apt-get install unzip curl -y && \
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -q awscliv2.zip
 ./aws/install
-cd .. && rm -rf ${AWSCLITMP}
+cd /tmp/ && rm -rf ${AWSCLITMP}
 apt-file update;
